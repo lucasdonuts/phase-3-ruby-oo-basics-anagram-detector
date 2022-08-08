41887927492badfaace70fc59518cd5adf818fdc
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-08 16:30:58 -0500

    commit tree ad363655d22575aa9f903e739316cc8533470848

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize( word )
+    @word = word
+  end
+
+  def match( arr )
+    arr.select do | word |
+      word.chars.sort == self.word.chars.sort
+    end
+  end
+
+end
